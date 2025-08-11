74e66c95f122169a9dd47e8e981dac50af851497
Iamcoder101 <141572881+Iamcoder101@users.noreply.github.com>
2025-08-11 06:37:03 +0530

    Create Reverse_bits.java

---
+++ binary/Reverse_bits.java
@@ -0,0 +1,66 @@
+/*
+LEET CODE 190
+
+190. Reverse Bits
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Reverse bits of a given 32 bits unsigned integer.
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation.
+ 
+
+Example 1:
+
+Input: n = 43261596
+
+Output: 964176192
+
+Explanation:
+
+Integer	Binary
+43261596	00000010100101000001111010011100
+964176192	00111001011110000010100101000000
+Example 2:
+
+Input: n = 2147483644
+
+Output: 1073741822
+
+Explanation:
+
+Integer	Binary
+2147483644	01111111111111111111111111111100
+1073741822	00111111111111111111111111111110
+ 
+
+Constraints:
+
+0 <= n <= 231 - 2
+n is even.
+ 
+
+Follow up: If this function is called many times, how would you optimize it?
+*/
+
+import java.lang.StringBuilder; 
+class Solution {
+    public int reverseBits(int n) {
+        String s=Integer.toBinaryString(n);
+        StringBuilder k=new StringBuilder(s);
+        if(s.length()!=32){
+            int len=32-s.length();
+            for(int i=0;i<len;i++){
+                k.insert(0,"0");
+            }
+        }
+        k.reverse();
+        int d=Integer.parseInt(k.toString(),2);
+        return d;
+    }
+}
